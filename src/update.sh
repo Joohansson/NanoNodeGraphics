5e1a42a4ca83700c96f40544be1137891eb6d832
joohansson <d84@outlook.com>
2018-10-26 09:06:19 +0200

    Create update.sh

---
+++ src/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Copy source files to netdata directories
+echo "Copy plugin"
+cp nanonode.chart.py /usr/libexec/netdata/python.d/
+echo "Copy plugin config to default location"
+cp nanonode.conf /usr/lib/netdata/conf.d/python.d
+echo "Copy dashboard"
+cp nano.html /usr/share/netdata/web
+echo "Copy dashboard style"
+cp nano.css /usr/share/netdata/web
+echo "Set netdata read access for dashboard and style"
+chown -R netdata:netdata /usr/share/netdata/web/nano.html
+chown -R netdata:netdata /usr/share/netdata/web/nano.html
